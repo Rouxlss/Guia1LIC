9e707256f78df5e91d0aeffb731d83b94bbec43a
Arnoldo <arnol.ortizjs@gmail.com>
2020-07-27 01:05:39 -0600

    Guia2

--- html/2.0/EC5/script.js
+++ html/2.0/EC5/script.js
@@ -1,20 +1,39 @@
 const elementos = ['Fuego', 'Aire', 'Agua', 'Tierra'];
+var push = document.querySelector('.push');
+var pop = document.querySelector('.pop');
+var content;
 
-document.querySelector('.signo1').addEventListener('click', function(){
-    document.querySelector('.ejemplo1').innerHTML = elementos.join('-');
+var array = document.querySelector('.array');
+var array2 = document.querySelector('.array2');
+
+var New;
+var ex;
+
+const e = () => {
+
+    content="   const Array = [";
+    for (i=0; i<elementos.length; i++){
+        content+=`'${elementos[i]}',`;
+    }
+
+    content+= "];";
+
+    content = content.replace(",];", "];");
+
+    array.innerHTML = content;
+    array2.innerHTML = content;
+
+}
+
+push.addEventListener('click', ()=> {
+    New = prompt('Ingresa el nuevo elemento: ');
+    elementos.push(New);
+    e();
 });
 
-document.querySelector('.signo2').addEventListener('click', function(){
-    document.querySelector('.ejemplo1').innerHTML = elementos.join('+');
+
+pop.addEventListener('click', ()=> {
+    elementos.pop();
+    e();
 });
 
-document.querySelector('.signo3').addEventListener('click', function(){
-    document.querySelector('.ejemplo1').innerHTML = elementos.join(',');
-});
-
-const verduras = ['broccoli', 'coliflor', 'zanahoria', 'lechuga', 'tomate'];
-
-document.querySelector('.button').addEventListener('click', function(){
-    document.querySelector('.ejemplo2_1').innerHTML = "Último elemento: " + `"${verduras[verduras.length-1]}"`;
-    document.querySelector('.ejemplo2').innerHTML = "Array: ['broccoli', 'coliflor', 'zanahoria', 'lechuga'];";
-});
