73e3dba0b318b88909e5c9f407f1dd1da45c9e7a
Arnoldo <arnol.ortizjs@gmail.com>
2020-08-24 00:04:27 -0600

    GUIA 5

---
+++ html/4.0/ejercicios/diccionario/js/index.js
@@ -0,0 +1,103 @@
+var input = document.querySelector('input.campo');
+
+var select = document.querySelector('.select select');
+
+var options = '';
+var regEx;
+var opciones;
+
+var textarea = document.querySelector('textarea');
+
+var palabras = [
+    /*10*/'Abstraction', 'Acoplamiento', 'Agregación', 'Algoritmo', 'Ambito de clase','Análisis', 'Aplicación', 'Array','Egolatría','Criollo',
+    /*20*/'Elucubración', 'Calificación', 'Capacidad','Evaluación final','Evidencias','Examen oral','Incidentes críticos','Instructor','Interés','Mentoría',
+    /*30*/ 'Monitorización','Tutor','Temblor','Prueba objetiva','Profesores noveles','Metodo','Atributos','Glosario','Galeón','Galaxia',
+    /*40*/ 'país','paciencia','zafiro','zaguán','tamarindo','tambor','vacaciones','vacuna','valeriana','nacer',
+    /*50*/ 'nación',
+];
+
+var definiciones = [
+    'Propiedad y/o técnica de software que oculta los detalles de la implementación.',
+    'Medida del grado en el que un objeto o componente depende de otro.',
+    'Relación en la que un objeto se compone o está construido de uno o más objetos, de modo que la colección completa representa un todo.',
+    'Método que describe cómo se resuelve un problema en término de las acciones que se ejecutan y especifica el orden en que se ejecutan estas acciones. ',
+    'Las variables privadas definidas fuera de los métodos internos a la clase tienen ámbito de clase.',
+    'Proceso de identificación, modelado y descripción de lo que hace un sistema y de cómo trabaja',
+    'Programa autónomo Java tal como cualquier programa escrito utilizando un lenguaje de alto nivel',
+    'Objeto contenedor que almacena una secuencia indexada de los mismos tipos de datos.',
+    'Culto y veneración que una persona se profesa a sí mismo.',
+    'Que es característico de la cultura y la tradición de un país hispanoamericano.',
+    'Pensamiento o reflexión sobre algo conseguido tras un intenso trabajo intelectual.',
+    'Expresión numérica o nominal que genera normalmente un profesor',
+    'Habilidad genera',
+    'Comprobación de los resultados logrados al final de un proceso y valoración',
+    'Actuaciones observables o trabajos presentados por los alumnos para ser evaluados.',
+    'Prueba para determinar el nivel de aprendizaje de los estudiantes',
+    'Hechos o acontecimientos que resultan especialmente relevantes',
+    'Se dice del docente que actúa pretendiendo sólo “llenar la cabeza” del estudiante de contenidos teóricos. ',
+    'Señala una tendencia observable en la conducta de una persona a preferir',
+    'Acción tutorial desarrollada por compañeros. ',
+    'Seguimiento periódico del trabajo del alumno para corregir errores',
+    'Docente que se responsabiliza de realizar la acción tutorial con un grupo de estudiantes. ',
+    'movimiento sismico',
+    'Modalidad de prueba escrita usada para la evaluación del aprendizaje',
+    'Profesores recién incorporados a la docencia universitaria. ',
+    'Algoritmo asociado a un objeto (o a una clase de objetos), cuya ejecución se desencadena tras la recepción de un "mensaje". ',
+    'características que tiene la clase',
+    'Lista breve en la que se explica el significado de las palabras más difíciles o más importantes de un texto.',
+    'Conjunto de estrellas, planetas, polvo interestelar, gases y partículas que giran alrededor de un centro y ocupan una parte del universo.',
+    'Barco antiguo grande de vela con 3 o 4 palos diseñado por los españoles.',
+    'Área geográfica que tiene su propio gobierno y sus leyes.',
+    'Capacidad de aguantar situaciones desagradables, dolorosas, ofensivas o pesadas sin quejarse',
+    'Piedra preciosa de color azul. Es tan valiosa como los diamantes.',
+    'Patio cubierto en la entrada de algunas casas. Está situado al entrar por la puerta de la calle.',
+    'Árbol tropical de tronco grueso y muy alto. La corteza es gris. Tiene flores de color amarillo',
+    'Instrumento musical de percusión.',
+    'Tiempo de descanso de una actividad habitual, como el trabajo o los estudios.',
+    'Líquido que los médicos inyectan a una persona o a un animal para evitar enfermedades. ',
+    'Planta de tallo recto y hojas puntiagudas.',
+    'Salir una persona o animal del vientre de su madre. Salir un animal de un huevo o una planta de una ...',
+    'Grupo de personas que viven en un mismo territorio y con un mismo gobierno',
+]
+
+// for(let i = 0;i<palabras.length;i++) {
+//     options += `<option value='${i}'>${palabras[i]}</option>`;
+// }
+// select.innerHTML = options;
+select.size = select.length;
+
+const mostrarDefinicion = () => {
+    opciones.forEach(opcion => {
+        opcion.addEventListener('click', ()=> {
+            textarea.value = definiciones[opcion.value];
+            console.log(opcion.value);
+        });
+    });
+};
+
+const limpiarLista = () => {
+    options = '';
+    select.innerHTML = options;
+};
+
+input.addEventListener('keyup', ()=> {
+
+    limpiarLista();
+    regEx = new RegExp(`${input.value}`, 'gim');
+    console.log(regEx);
+    if((input.value).length == 0){
+        textarea.value = '';
+    }else {
+        for(let i = 0;i<palabras.length;i++) {
+            if(regEx.test(palabras[i])) {
+                options += `<option value='${i}'>${palabras[i]}</option>`;
+            }
+        }
+    }
+    select.innerHTML = options;
+    select.size = select.length;
+
+    opciones = document.querySelectorAll('.select select option');
+    mostrarDefinicion();
+
+});
---
+++ html/4.0/ejercicios/validaciones/js/script.js
@@ -0,0 +1,95 @@
+var select = document.querySelector('select');
+var opciones = document.querySelectorAll('option');
+
+var boton = document.querySelector('button');
+var input = document.querySelector('.campo');
+
+var mensaje = 'Formato incorrecto';
+valido = false;
+
+var x = 0;
+
+select.addEventListener('change', ()=> {
+    if(select.value == 0){
+        input.value = '';
+        input.placeholder = '###.###.###.###';
+        console.log('ip');
+        input.addEventListener('keyup', ()=> {
+            validarIP();
+        });
+        input.addEventListener('keypress', ()=> {
+            validarIP();
+        });
+    }else if (select.value == 1){
+        input.value = '';
+        input.placeholder = 'protocolo://www.nombredelsitio.com';
+        console.log('url');
+        input.addEventListener('keyup', ()=> {
+            validarWEB();
+        });
+        input.addEventListener('keypress', ()=> {
+            validarWEB();
+        });
+    }else if (select.value == 2){
+        input.value = '';
+        input.placeholder = 'alguien@example.com';
+        console.log('correo');
+        input.addEventListener('keyup', ()=> {
+            validarCORREO();
+        });
+        input.addEventListener('keypress', ()=> {
+            validarCORREO();
+        });
+    }
+});
+
+const validarIP = () => {
+    console.log(input.value);
+    regEx = /^[1-2]{1}[1-9]{2}.?[1-2]{1}[1-9]{2}.?[1-2]{1}[1-9]{2}.?[1-2]{1}[1-9]{2}$/;
+    if(regEx.test(input.value)){
+        valido = true;
+    }else {
+        mensaje = `El formato: ${input.value} es invalido`;
+        valido = false;
+    }
+}
+
+const validarWEB = () => {
+    console.log(input.value);
+    regEx = /^(http:\/\/|https:\/\/|ftp:\/\/)(www).?\w.?\w/;
+    if(regEx.test(input.value)){
+        valido = true;
+    }else {
+        mensaje = `El formato: ${input.value} es invalido`;
+        valido = false;
+    }
+}
+
+const validarCORREO = () => {
+    console.log(input.value);
+    regEx = /\w(@)\D.?\D/;
+    if(regEx.test(input.value)){
+        valido = true;
+    }else {
+        mensaje = `El formato: ${input.value} es invalido`;
+        valido = false;
+    }
+}
+
+input.addEventListener('keydown', ()=> {
+    if (event.key === "Enter") {
+        if(valido == true){
+            alert('Formato correcto!');
+        }else {
+            alert(mensaje);
+        }
+    }
+});
+
+boton.addEventListener('click', ()=> {
+    if(valido == true){
+        alert('Formato correcto!');
+    }else {
+        alert(mensaje);
+    }
+});
